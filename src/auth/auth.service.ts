a9ad42eef5ff1bc43b624e4d9a31d4f2612995bf
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-10 23:45:15 +0600

    feat: working on user validation in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { AuthPayloadDto } from './dtos/Auth.dto';
+
+const fakeUsers = [
+  {
+    id: 1,
+    username: 'tousif',
+    password: '123456',
+  },
+  {
+    id: 2,
+    username: 'john',
+    password: 'password123',
+  },
+];
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  validateUser(authPayload: AuthPayloadDto) {
+    const { username, password } = authPayload;
+    const findUser = fakeUsers.find((user) => user.username === username);
+
+    if (!findUser) return null;
+
+    if(password === findUser.password) {
+    }
+  }
+}
