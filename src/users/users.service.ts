1f510ffc16adbe390a51cc465497132f716217b0
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-11 09:27:29 +0600

    feat: implement UsersService with createUser method for user registration and add UserResponse type

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UserResponse } from 'src/utils/types/user-response.type';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<UserResponse> {
+    const { email, password } = createUserDto;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await this.userRepository.create({
+      email: email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await this.userRepository.save(newUser);
+
+    const { password: _, ...result } = savedUser;
+    return result;
+  }
+}
---
+++ src/utils/types/user-response.type.ts
@@ -0,0 +1,4 @@
+export type UserResponse = {
+  id: number;
+  email: string;
+};
